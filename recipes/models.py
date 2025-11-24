b27ce513d63f3d97a50abbef12d98e243c85fd7a
aliya-ya-ya <aliya.karimova.12345.7@gmail.com>
2025-11-24 20:32:14 +0300

    first commit

---
+++ recipes/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    cooking_time = models.PositiveIntegerField(help_text="В минутах")
+    description = models.TextField()
+    img = models.ImageField(upload_to='recipes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    unit = models.CharField(max_length=50)
+    quantity = models.FloatField()
+
+    def __str__(self):
+        return f"{self.quantity} {self.unit} {self.ingredient.name}"
---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+def about(request):
+    return render(request, 'recipes/about.html')
